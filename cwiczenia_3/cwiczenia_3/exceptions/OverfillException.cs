32eccf644d1558aea6775c955f7e871e24a8d5ff
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-19 11:16:46 +0100

    Added constructors to OverfillException

--- cwiczenia_3/cwiczenia_3/exceptions/OverfillException.cs
+++ cwiczenia_3/cwiczenia_3/exceptions/OverfillException.cs
@@ -2,5 +2,15 @@
 
 public class OverfillException : Exception
 {
-    
+    public OverfillException()
+    {
+    }
+
+    public OverfillException(string? message) : base(message)
+    {
+    }
+
+    public OverfillException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
