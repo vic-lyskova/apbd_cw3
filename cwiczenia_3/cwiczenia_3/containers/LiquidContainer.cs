c16eb9efa9d4fa4f14e7b20f7bdf6677aec22f2f
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-22 21:01:42 +0100

    Added LiquidContainer class which extends Container and implements IHazardNotifier

---
+++ cwiczenia_3/cwiczenia_3/containers/LiquidContainer.cs
@@ -0,0 +1,20 @@
+using cwiczenia_3.interfaces;
+
+namespace cwiczenia_3.containers;
+
+public class LiquidContainer : Container, IHazardNotifier
+{
+    public LiquidContainer(double cargoWeight, double height, double weight, double depth, string containerType, double maxLoad) : base(cargoWeight, height, weight, depth, containerType, maxLoad)
+    {
+    }
+
+    public override void Load(double cargoWeight)
+    {
+        base.Load(cargoWeight);
+    }
+
+    public void Notify()
+    {
+        throw new NotImplementedException();
+    }
+}
