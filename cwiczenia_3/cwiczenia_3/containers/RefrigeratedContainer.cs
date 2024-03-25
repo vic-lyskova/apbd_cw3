13788f624800ac1bf88b2431c2865c0a4640c0cf
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-25 21:34:56 +0100

    Added field Product and Temperature and required logic in constructor

--- cwiczenia_3/cwiczenia_3/containers/RefrigeratedContainer.cs
+++ cwiczenia_3/cwiczenia_3/containers/RefrigeratedContainer.cs
@@ -1,8 +1,76 @@
+using cwiczenia_3.enums;
+
 namespace cwiczenia_3.containers;
 
 public class RefrigeratedContainer : Container
 {
-    public RefrigeratedContainer(double cargoWeight, double height, double weight, double depth, string containerType, double maxLoad) : base(cargoWeight, height, weight, depth, containerType, maxLoad)
+    public ProductType Product { get; set; }
+    public double Temperature { get; set; }
+    
+    public RefrigeratedContainer(double cargoWeight, double height, double weight, double depth, string containerType, double maxLoad, ProductType product, double temperature) 
+        : base(cargoWeight, height, weight, depth, containerType, maxLoad)
     {
+        Product = product;
+        double temp = 0;
+        switch (product)
+        {
+            case ProductType.Bananas:
+            {
+                temp = 13.3;
+                break;
+            }
+            case ProductType.Chocolate:
+            {
+                temp = 18;
+                break;
+            }
+            case ProductType.Fish:
+            {
+                temp = 2;
+                break;
+            }
+            case ProductType.Meat:
+            {
+                temp = -15;
+                break;
+            }
+            case ProductType.IceCream:
+            {
+                temp = -18;
+                break;
+            }
+            case ProductType.FrozenPizza:
+            {
+                temp = -30;
+                break;
+            }
+            case ProductType.Cheese:
+            {
+                temp = 7.2;
+                break;
+            }
+            case ProductType.Sausages:
+            {
+                temp = 5;
+                break;
+            }
+            case ProductType.Butter:
+            {
+                temp = 20.5;
+                break;
+            }
+            case ProductType.Eggs:
+            {
+                temp = 19;
+                break;
+            }
+        }
+
+        if (temperature < temp)
+        {
+            temperature = temp;
+        }
+
+        Temperature = temperature;
     }
 }
