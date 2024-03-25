00f62f475627cf20941a47a48ea36537cb1c990b
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-25 21:05:48 +0100

    Added class GasContainer which implements IHazardNotifier and extends Container

---
+++ cwiczenia_3/cwiczenia_3/containers/GasContainer.cs
@@ -0,0 +1,20 @@
+using cwiczenia_3.interfaces;
+
+namespace cwiczenia_3.containers;
+
+public class GasContainer : Container, IHazardNotifier
+{
+    public GasContainer(double cargoWeight, double height, double weight, double depth, string containerType, double maxLoad) : base(cargoWeight, height, weight, depth, containerType, maxLoad)
+    {
+    }
+
+    public override void Unload()
+    {
+        base.Unload();
+    }
+
+    public void Notify(string serialNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
