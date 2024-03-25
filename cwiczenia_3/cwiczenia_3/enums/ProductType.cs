478fba4f15950ca1f4962941c49ef8832af05f19
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-25 21:26:11 +0100

    Changed the name according to the given data

--- cwiczenia_3/cwiczenia_3/enums/ProductType.cs
+++ cwiczenia_3/cwiczenia_3/enums/ProductType.cs
@@ -2,6 +2,6 @@ namespace cwiczenia_3.enums;
 
 public enum ProductType
 {
-    Banana, Chocolate, Fish, Meat, IceCream, 
+    Bananas, Chocolate, Fish, Meat, IceCream, 
     FrozenPizza, Cheese, Sausages, Butter, Eggs
 }
