c0db6f19909634519d9b3176737c972b544fbf47
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-25 21:43:03 +0100

    Added fields Containers, MaxSpeed, MaxContainers and MaxContainersWeight and constructor

--- cwiczenia_3/cwiczenia_3/ships/Ship.cs
+++ cwiczenia_3/cwiczenia_3/ships/Ship.cs
@@ -1,6 +1,19 @@
+using cwiczenia_3.containers;
+
 namespace cwiczenia_3.ships;
 
 public class Ship
 {
-    
+    public HashSet<Container> Containers { get; set; }
+    public double MaxSpeed { get; set; }
+    public int MaxContainers { get; set; }
+    public double MaxContainersWeight { get; set; }
+
+    public Ship(HashSet<Container> containers, double maxSpeed, int maxContainers, double maxContainersWeight)
+    {
+        Containers = containers;
+        MaxSpeed = maxSpeed;
+        MaxContainers = maxContainers;
+        MaxContainersWeight = maxContainersWeight;
+    }
 }
