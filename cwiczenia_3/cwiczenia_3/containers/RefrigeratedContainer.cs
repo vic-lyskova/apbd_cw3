9a6ac67bd7c233777f4a27b3dbd9f539a4bd314a
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-25 21:17:06 +0100

    Added RefrigeratedContainer class which extends Container

---
+++ cwiczenia_3/cwiczenia_3/containers/RefrigeratedContainer.cs
@@ -0,0 +1,8 @@
+namespace cwiczenia_3.containers;
+
+public class RefrigeratedContainer : Container
+{
+    public RefrigeratedContainer(double cargoWeight, double height, double weight, double depth, string containerType, double maxLoad) : base(cargoWeight, height, weight, depth, containerType, maxLoad)
+    {
+    }
+}
